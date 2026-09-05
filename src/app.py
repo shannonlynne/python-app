eb8fe59c933e4f362585382b2a23a20919310713
Shannon Morrell <shannon.morrell2@gmail.com>
2026-09-05 15:56:27 -0500

    ugh

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
     	'hostname': socket.gethostname(),
-        'message': 'Backstage yet?!?!'
+        'message': 'Backstage yet?!?!!!'
     })
 
 @app.route('/api/v1/healthz')
