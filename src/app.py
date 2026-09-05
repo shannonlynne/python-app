616b4487a30ff16ebf264846f1922b88ebaaeaaa
Shannon Morrell <shannon.morrell2@gmail.com>
2026-09-05 15:37:46 -0500

    Update CI/CD workflow and Python app

--- src/app.py
+++ src/app.py
@@ -10,8 +10,7 @@ def details():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
     	'hostname': socket.gethostname(),
-        'message': 'You go girl!',
-        'question': 'When do we get to BackStage?'
+        'message': 'You go girl!'
     })
 
 @app.route('/api/v1/healthz')
